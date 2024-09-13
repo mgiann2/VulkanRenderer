d4d454989f91547fdc71abc97746cbd52549bceb
Matteo Giannone <matteogiannone24@gmail.com>
2024-09-13 11:48:55 -0400

    Create instance

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Silk.NET.Maths;
+﻿using System.Runtime.InteropServices;
+using Silk.NET.Core;
+using Silk.NET.Maths;
+using Silk.NET.Vulkan;
 using Silk.NET.Windowing;
 
 var app = new MGSVRenderingApp();
@@ -10,6 +13,9 @@ unsafe class MGSVRenderingApp
     const int HEIGHT = 1080;
 
     private IWindow? window;
+    private Vk? vk;
+
+    private Instance instance;
 
     public void Run()
     {
@@ -39,7 +45,7 @@ unsafe class MGSVRenderingApp
 
     private void InitVulkan()
     {
-
+        CreateInstance();
     }
 
     private void MainLoop()
@@ -50,5 +56,40 @@ unsafe class MGSVRenderingApp
     private void CleanUp()
     {
         window?.Dispose();
+    }
+
+    private void CreateInstance()
+    {
+        vk = Vk.GetApi();
+
+        ApplicationInfo appInfo = new()
+        {
+            SType = StructureType.ApplicationInfo,
+            PApplicationName = (byte*)Marshal.StringToHGlobalAnsi("MGSV Renderer"),
+            ApplicationVersion = new Version32(1, 0, 0),
+            PEngineName = (byte*)Marshal.StringToHGlobalAnsi("No Engine"),
+            EngineVersion = new Version32(1, 0, 0),
+            ApiVersion = new Version32(1, 0, 0)
+        };
+
+        InstanceCreateInfo createInfo = new()
+        {
+            SType = StructureType.InstanceCreateInfo,
+            PApplicationInfo = &appInfo
+        };
+
+        var glfwExtensions = window!.VkSurface!.GetRequiredExtensions(out var glfwExtensionCount);
+
+        createInfo.EnabledExtensionCount = glfwExtensionCount;
+        createInfo.PpEnabledExtensionNames = glfwExtensions;
+        createInfo.EnabledLayerCount = 0;
+
+        if (vk.CreateInstance(ref createInfo, null, out instance) != Result.Success)
+        {
+            throw new Exception("Failed to create instance.");
+        }
+
+        Marshal.FreeHGlobal((IntPtr)appInfo.PApplicationName);
+        Marshal.FreeHGlobal((IntPtr)appInfo.PEngineName);
     }
 }
