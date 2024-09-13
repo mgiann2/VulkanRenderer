2f947466b126aa0d067126f6dafda25f270b073e
Matteo Giannone <matteogiannone24@gmail.com>
2024-09-13 13:52:40 -0400

    Add validation layers

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Runtime.InteropServices;
 using Silk.NET.Core;
+using Silk.NET.Core.Native;
 using Silk.NET.Maths;
 using Silk.NET.Vulkan;
+using Silk.NET.Vulkan.Extensions.EXT;
 using Silk.NET.Windowing;
 
 var app = new MGSVRenderingApp();
@@ -12,10 +14,20 @@ unsafe class MGSVRenderingApp
     const int WIDTH = 1920;
     const int HEIGHT = 1080;
 
+    bool EnableValidationLayers = true;
+
+    private readonly string[] validationLayers = new[]
+    {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
     private IWindow? window;
     private Vk? vk;
 
     private Instance instance;
+
+    private ExtDebugUtils? debugUtils;
+    private DebugUtilsMessengerEXT debugMessenger;
 
     public void Run()
     {
@@ -46,6 +58,7 @@ unsafe class MGSVRenderingApp
     private void InitVulkan()
     {
         CreateInstance();
+        SetupDebugMessenger();
     }
 
     private void MainLoop()
@@ -55,6 +68,11 @@ unsafe class MGSVRenderingApp
 
     private void CleanUp()
     {
+        if (EnableValidationLayers)
+        {
+            debugUtils!.DestroyDebugUtilsMessenger(instance, debugMessenger, null);
+        }
+
         vk!.DestroyInstance(instance, null);
         vk!.Dispose();
 
@@ -64,6 +82,11 @@ unsafe class MGSVRenderingApp
     private void CreateInstance()
     {
         vk = Vk.GetApi();
+
+        if (EnableValidationLayers && !CheckValidationLayerSupport())
+        {
+            throw new Exception("Validation layers requested, but not available!");
+        }
 
         ApplicationInfo appInfo = new()
         {
@@ -81,11 +104,24 @@ unsafe class MGSVRenderingApp
             PApplicationInfo = &appInfo
         };
 
-        var glfwExtensions = window!.VkSurface!.GetRequiredExtensions(out var glfwExtensionCount);
+        var extensions = GetRequiredExtensions();
+        createInfo.EnabledExtensionCount = (uint) extensions.Length;
+        createInfo.PpEnabledExtensionNames = (byte**) SilkMarshal.StringArrayToPtr(extensions);
 
-        createInfo.EnabledExtensionCount = glfwExtensionCount;
-        createInfo.PpEnabledExtensionNames = glfwExtensions;
-        createInfo.EnabledLayerCount = 0;
+        if (EnableValidationLayers)
+        {
+            createInfo.EnabledLayerCount = (uint)validationLayers.Length;
+            createInfo.PpEnabledLayerNames = (byte**)SilkMarshal.StringArrayToPtr(validationLayers);
+
+            DebugUtilsMessengerCreateInfoEXT debugCreateInfo = new();
+            PopulateDebugMessengerCreateInfo(ref debugCreateInfo);
+            createInfo.PNext = &debugCreateInfo;
+        }
+        else
+        {
+            createInfo.EnabledLayerCount = 0;
+            createInfo.PNext = null;
+        }
 
         if (vk.CreateInstance(ref createInfo, null, out instance) != Result.Success)
         {
@@ -94,5 +130,77 @@ unsafe class MGSVRenderingApp
 
         Marshal.FreeHGlobal((IntPtr)appInfo.PApplicationName);
         Marshal.FreeHGlobal((IntPtr)appInfo.PEngineName);
+        SilkMarshal.Free((nint)createInfo.PpEnabledExtensionNames);
+
+        if (EnableValidationLayers)
+        {
+            SilkMarshal.Free((nint)createInfo.PpEnabledLayerNames);
+        }
+    }
+
+    private void PopulateDebugMessengerCreateInfo(ref DebugUtilsMessengerCreateInfoEXT createInfo)
+    {
+        createInfo.SType = StructureType.DebugUtilsMessengerCreateInfoExt;
+        createInfo.MessageSeverity = DebugUtilsMessageSeverityFlagsEXT.VerboseBitExt |
+                                     DebugUtilsMessageSeverityFlagsEXT.WarningBitExt |
+                                     DebugUtilsMessageSeverityFlagsEXT.ErrorBitExt;
+        createInfo.MessageType = DebugUtilsMessageTypeFlagsEXT.GeneralBitExt |
+                                 DebugUtilsMessageTypeFlagsEXT.PerformanceBitExt |
+                                 DebugUtilsMessageTypeFlagsEXT.ValidationBitExt;
+        createInfo.PfnUserCallback = (DebugUtilsMessengerCallbackFunctionEXT) DebugCallback;
+    }
+
+    private void SetupDebugMessenger()
+    {
+        if (!EnableValidationLayers) return;
+
+        if (!vk!.TryGetInstanceExtension(instance, out debugUtils)) return;
+
+        DebugUtilsMessengerCreateInfoEXT createInfo = new();
+        PopulateDebugMessengerCreateInfo(ref createInfo);
+
+        if (debugUtils!.CreateDebugUtilsMessenger(instance, in createInfo, null, out debugMessenger) != Result.Success)
+        {
+            throw new Exception("Failed to set up debug messenger");
+        }
+    }
+
+    private string[] GetRequiredExtensions()
+    {
+        var glfwExtensions = window!.VkSurface!.GetRequiredExtensions(out var glfwExtensionCount);
+        var extensions = SilkMarshal.PtrToStringArray((nint)glfwExtensions, (int)glfwExtensionCount);
+        
+        if (EnableValidationLayers)
+        {
+            extensions.Append(ExtDebugUtils.ExtensionName).ToArray();
+        }
+
+        return extensions;
+    }
+
+    private bool CheckValidationLayerSupport()
+    {
+        uint layerCount = 0;
+        vk!.EnumerateInstanceLayerProperties(ref layerCount, null);
+        var availableLayers = new LayerProperties[layerCount];
+        fixed (LayerProperties* availableLayersPtr = availableLayers)
+        {
+            vk!.EnumerateInstanceLayerProperties(ref layerCount, availableLayersPtr);
+        }
+
+        var availableLayerNames = availableLayers.Select(layer => Marshal.PtrToStringAnsi((IntPtr)layer.LayerName)).ToHashSet();
+
+        return validationLayers.All(availableLayerNames.Contains);
+    }
+
+    private uint DebugCallback(
+            DebugUtilsMessageSeverityFlagsEXT messageSeverity,
+            DebugUtilsMessageTypeFlagsEXT messageType,
+            DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+            void* pUserData)
+    {
+        Console.WriteLine($"Validation layer: {Marshal.PtrToStringAnsi((nint) pCallbackData->PMessage)}");
+
+        return Vk.False;
     }
 }
