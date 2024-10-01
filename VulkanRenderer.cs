3f0c0f868a262487db20f3d58a5cd74d4c7db33a
Matteo <matteogiannone24@gmail.com>
2024-10-01 16:23:04 -0400

    Add function to pick physical device in vulkan renderer

--- VulkanRenderer.cs
+++ VulkanRenderer.cs
@@ -5,6 +5,7 @@ using Silk.NET.Core.Native;
 using Silk.NET.Vulkan;
 using Silk.NET.Vulkan.Extensions.EXT;
 using Silk.NET.Vulkan.Extensions.KHR;
+using Silk.NET.Windowing;
 
 unsafe class VulkanRenderer : IDisposable
 {
@@ -21,19 +22,27 @@ unsafe class VulkanRenderer : IDisposable
     };
 
     private Vk vk;
-    private IVkSurface surface;
+    private IVkSurface windowSurface;
     private Instance instance;
+
+    private KhrSurface khrSurface;
+    private SurfaceKHR surface;
+
+    private PhysicalDevice physicalDevice;
 
     private bool disposedValue;
 
-    public VulkanRenderer(IVkSurface surface, bool enableValidationLayers = false)
+    public VulkanRenderer(IVkSurface windowSurface, bool enableValidationLayers = false)
     {
         vk = Vk.GetApi();
-        this.surface = surface;
+        this.windowSurface = windowSurface;
 
         EnableValidationLayers = enableValidationLayers;
         
         instance = CreateInstance();
+        (khrSurface, surface) = CreateSurface();
+        physicalDevice = PickPhysicalDevice();
+
     }
 
     /// <summary>
@@ -115,9 +124,140 @@ unsafe class VulkanRenderer : IDisposable
         return newInstance;
     }
 
+    private (KhrSurface, SurfaceKHR) CreateSurface()
+    {
+        if (!vk.TryGetInstanceExtension(instance, out KhrSurface khrSurface))
+        {
+            throw new NotSupportedException("KHR_surface extension not found!");
+        }
+        
+        var surface =windowSurface.Create<AllocationCallbacks>(instance.ToHandle(), null).ToSurface();
+        return (khrSurface, surface);     
+    }
+
+    private PhysicalDevice PickPhysicalDevice()
+    {
+        var devices = vk.GetPhysicalDevices(instance);
+
+        foreach (var device in devices)
+        {
+            if (IsSuitablePhysicalDevice(device))
+            {
+                return device;
+            }
+        }
+
+        throw new Exception("Unable to find suitable physical device!");
+    }
+
+    private bool IsPhysicalDeviceSuitable(PhysicalDevice physicalDevice)
+    {
+        var indices = FindQueueFamilies(physicalDevice);
+
+        bool extensionsSupported = CheckDeviceExtensionSupport(physicalDevice);
+
+        bool swapChainAdequate = false;
+        if (extensionsSupported)
+        {
+            var swapChainSupportDetails = QuerySwapChainSupport(physicalDevice);
+            swapChainAdequate = swapChainSupportDetails.PresentModes.Any() && swapChainSupportDetails.Formats.Any();
+        }
+
+        return indices.IsComplete() && extensionsSupported && swapChainAdequate;
+    }
+
+    private QueueFamilyIndices FindQueueFamilies(PhysicalDevice physicalDevice)
+    {
+        QueueFamilyIndices indices = new();
+
+        uint queueFamilyCount = 0;
+        vk.GetPhysicalDeviceQueueFamilyProperties(physicalDevice, ref queueFamilyCount, null);
+
+        var queueFamilies = new QueueFamilyProperties[queueFamilyCount];
+        fixed (QueueFamilyProperties* queueFamiliesPtr = queueFamilies)
+        {
+            vk.GetPhysicalDeviceQueueFamilyProperties(physicalDevice, ref queueFamilyCount, queueFamiliesPtr);
+        }
+        
+        uint i = 0;
+        foreach (var queueFamily in queueFamilies)
+        {
+            if (queueFamily.QueueFlags.HasFlag(QueueFlags.GraphicsBit))
+            {
+                indices.GraphicsFamily = i;
+            }
+
+            if (indices.IsComplete())
+            {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    private bool CheckDeviceExtensionSupport(PhysicalDevice physicalDevice)
+    {
+        uint extensionCount = 0;
+        vk.EnumerateDeviceExtensionProperties(physicalDevice, (byte*) null, ref extensionCount, null);
+
+        var availableExtensions = new ExtensionProperties[extensionCount];
+        fixed (ExtensionProperties* availableExtensionsPtr = availableExtensions)
+        {
+            vk.EnumerateDeviceExtensionProperties(physicalDevice, (byte*) null, ref extensionCount, availableExtensionsPtr);
+        }
+
+        var availableExtensionNames = availableExtensions.Select(extension => Marshal.PtrToStringAnsi((nint) extension.ExtensionName)).ToHashSet();
+
+        return deviceExtensions.All(availableExtensionNames.Contains);
+    }
+
+    private SwapChainSupportDetails QuerySwapChainSupport(PhysicalDevice physicalDevice)
+    {
+        var details = new SwapChainSupportDetails();
+
+        khrSurface.GetPhysicalDeviceSurfaceCapabilities(physicalDevice, surface, out details.Capabilities);
+
+        uint formatCount = 0;
+        khrSurface.GetPhysicalDeviceSurfaceFormats(physicalDevice, surface, ref formatCount, null);
+
+        if (formatCount != 0)
+        {
+            details.Formats = new SurfaceFormatKHR[formatCount];
+            fixed (SurfaceFormatKHR* formatsPtr = details.Formats)
+            {
+                khrSurface.GetPhysicalDeviceSurfaceFormats(physicalDevice, surface, ref formatCount, formatsPtr);
+            }
+        }
+        else 
+        {
+            details.Formats = Array.Empty<SurfaceFormatKHR>();
+        }
+
+        uint presentModeCount = 0;
+        khrSurface.GetPhysicalDeviceSurfacePresentModes(physicalDevice, surface, ref presentModeCount, null);
+
+        if (presentModeCount != 0)
+        {
+            details.PresentModes = new PresentModeKHR[presentModeCount];
+            fixed (PresentModeKHR* presentModesPtr = details.PresentModes)
+            {
+                khrSurface.GetPhysicalDeviceSurfacePresentModes(physicalDevice, surface, ref presentModeCount, presentModesPtr);
+            }
+        }
+        else
+        {
+            details.PresentModes = Array.Empty<PresentModeKHR>();
+        }
+
+        return details;
+    }
+
     private string[] GetRequiredExtensions()
     {
-        var glfwExtensions = surface.GetRequiredExtensions(out var glfwExtensionCount);
+        var glfwExtensions = windowSurface.GetRequiredExtensions(out var glfwExtensionCount);
         var extensions = SilkMarshal.PtrToStringArray((nint)glfwExtensions, (int)glfwExtensionCount);
         
         if (EnableValidationLayers)
