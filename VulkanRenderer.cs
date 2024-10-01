8eab3915f2a77ea99e685412b54410294c0551e5
Matteo Giannone <matteogiannone24@gmail.com>
2024-09-30 20:24:39 -0400

    Add create instance function to vulkan renderer

--- VulkanRenderer.cs
+++ VulkanRenderer.cs
@@ -1,8 +1,40 @@
+using System.Runtime.InteropServices;
+using Silk.NET.Core;
+using Silk.NET.Core.Contexts;
+using Silk.NET.Core.Native;
+using Silk.NET.Vulkan;
+using Silk.NET.Vulkan.Extensions.EXT;
+using Silk.NET.Vulkan.Extensions.KHR;
 
-
-class VulkanRenderer : IDisposable
+unsafe class VulkanRenderer : IDisposable
 {
+    private readonly bool EnableValidationLayers;
+
+    private readonly string[] validationLayers = new[]
+    {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    private readonly string[] deviceExtensions = new[]
+    {
+        KhrSwapchain.ExtensionName
+    };
+
+    private Vk vk;
+    private IVkSurface surface;
+    private Instance instance;
+
     private bool disposedValue;
+
+    public VulkanRenderer(IVkSurface surface, bool enableValidationLayers = false)
+    {
+        vk = Vk.GetApi();
+        this.surface = surface;
+
+        EnableValidationLayers = enableValidationLayers;
+        
+        instance = CreateInstance();
+    }
 
     /// <summary>
     /// Initializes renderer to start drawing new frame
@@ -21,6 +53,109 @@ class VulkanRenderer : IDisposable
 
     }
 
+    private Instance CreateInstance()
+    {
+        if (EnableValidationLayers && !CheckValidationLayerSupport())
+        {
+            throw new Exception("Requested validation layers, but not available!");
+        }
+
+        ApplicationInfo appInfo = new()
+        {
+            SType = StructureType.ApplicationInfo,
+            PApplicationName = (byte*) Marshal.StringToHGlobalAnsi("Vulkan Renderer"),
+            ApplicationVersion = new Version32(1, 0, 0),
+            PEngineName = (byte*) Marshal.StringToHGlobalAnsi("No Engine"),
+            EngineVersion = new Version32(1, 0, 0),
+            ApiVersion = Vk.Version13
+        };
+
+        var extensions = GetRequiredExtensions();
+
+        InstanceCreateInfo instanceInfo = new()
+        {
+            SType = StructureType.InstanceCreateInfo,
+            PApplicationInfo = &appInfo,
+            EnabledExtensionCount = (uint) extensions.Count(),
+            PpEnabledExtensionNames = (byte**) SilkMarshal.StringArrayToPtr(extensions),
+            EnabledLayerCount = 0,
+            PNext = null
+        };
+
+        if (EnableValidationLayers)
+        {
+            instanceInfo.EnabledLayerCount = (uint) validationLayers.Count();
+            instanceInfo.PpEnabledLayerNames = (byte**) SilkMarshal.StringArrayToPtr(validationLayers);
+
+            DebugUtilsMessengerCreateInfoEXT messengerInfo = new()
+            {
+                SType = StructureType.DebugUtilsMessengerCreateInfoExt,
+                MessageSeverity = DebugUtilsMessageSeverityFlagsEXT.VerboseBitExt |
+                                  DebugUtilsMessageSeverityFlagsEXT.WarningBitExt |
+                                  DebugUtilsMessageSeverityFlagsEXT.ErrorBitExt,
+                MessageType = DebugUtilsMessageTypeFlagsEXT.GeneralBitExt |
+                              DebugUtilsMessageTypeFlagsEXT.ValidationBitExt |
+                              DebugUtilsMessageTypeFlagsEXT.PerformanceBitExt,
+                PfnUserCallback = (PfnDebugUtilsMessengerCallbackEXT) DebugCallback
+            };
+            instanceInfo.PNext = &messengerInfo;
+        }
+
+        if (vk.CreateInstance(in instanceInfo, null, out var newInstance) != Result.Success)
+        {
+            throw new Exception("Failed to create instance!");
+        }
+
+        // free unmanaged memory
+        Marshal.FreeHGlobal((nint) appInfo.PEngineName);
+        Marshal.FreeHGlobal((nint) appInfo.PApplicationName);
+        SilkMarshal.Free((nint) instanceInfo.PpEnabledExtensionNames);
+        if (EnableValidationLayers) SilkMarshal.Free((nint) instanceInfo.PpEnabledLayerNames);
+
+        return newInstance;
+    }
+
+    private string[] GetRequiredExtensions()
+    {
+        var glfwExtensions = surface.GetRequiredExtensions(out var glfwExtensionCount);
+        var extensions = SilkMarshal.PtrToStringArray((nint)glfwExtensions, (int)glfwExtensionCount);
+        
+        if (EnableValidationLayers)
+        {
+            return extensions.Append(ExtDebugUtils.ExtensionName).ToArray();
+        }
+
+        return extensions;
+    }
+
+    private bool CheckValidationLayerSupport()
+    {
+        uint layerCount = 0;
+        vk.EnumerateInstanceLayerProperties(ref layerCount, null);
+        var availableLayers = new LayerProperties[layerCount];
+        fixed (LayerProperties* availableLayersPtr = availableLayers)
+        {
+            vk.EnumerateInstanceLayerProperties(ref layerCount, availableLayersPtr);
+        }
+
+        var availableLayerNames = availableLayers.Select(layer => Marshal.PtrToStringAnsi((nint) layer.LayerName)).ToHashSet();
+
+        return validationLayers.All(availableLayerNames.Contains);
+    }
+
+    private uint DebugCallback(
+            DebugUtilsMessageSeverityFlagsEXT messageSeverity,
+            DebugUtilsMessageTypeFlagsEXT messageType,
+            DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+            void* pUserData)
+    {
+        Console.WriteLine($"Validation layer: {Marshal.PtrToStringAnsi((nint) pCallbackData->PMessage)}");
+
+        return Vk.False;
+    }
+
+    // IDisposable methods
+    
     ~VulkanRenderer()
     {
         Dispose(disposing: false);
@@ -42,6 +177,8 @@ class VulkanRenderer : IDisposable
             }
 
             // free unmanaged resources unmanaged objects and override finalizer
+            vk.DestroyInstance(instance, null);
+
             disposedValue = true;
         }
     }
